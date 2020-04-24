286d06df96f925c5c937dd3d47ba7725a1d9a3f9
Thomas Wheeler <Thomas.T.Wheeler@aa.com>
2020-04-24 12:44:27 -0500

    extract test object

--- src/test/java/org/byteworks/calculator/CalculatorTests.java
+++ src/test/java/org/byteworks/calculator/CalculatorTests.java
@@ -9,11 +9,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class CalculatorTests {
+    private final Calculator calculator = new Calculator();
 
     @Test
     @DisplayName("1 + 1 = 2")
     void addsTwoNumbers() {
-        Calculator calculator = new Calculator();
         assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
     }
 
@@ -26,7 +26,6 @@ class CalculatorTests {
         "1,  100, 101" })
     // @formatter:on
     void add(int first, int second, int expectedResult) {
-        Calculator calculator = new Calculator();
         assertEquals(expectedResult, calculator.add(first, second),
                 () -> first + " + " + second + " should equal " + expectedResult);
     }
@@ -34,14 +33,12 @@ class CalculatorTests {
     @Test
     @DisplayName("6 / 2 = 3")
     void dividesTwoNumbers() {
-        Calculator calculator = new Calculator();
         assertEquals(3, calculator.divide(6, 2), "6 / 2 should equal 3");
     }
 
     @Test
     @DisplayName("6 / 0 = error")
     void dividesByZero() {
-        Calculator calculator = new Calculator();
         Assertions.assertThrows(ArithmeticException.class, () -> calculator.divide(6, 0),
                 "6 / 0 should throw an error");
     }
